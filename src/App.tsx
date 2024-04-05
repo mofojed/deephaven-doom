9d148e22429428f4de54dafd11c58fee73e43e8d
Mike Bender <mikebender@deephaven.io>
2024-04-04 22:18:24 -0400

    Hook it up to the grid
    - It's working! Huzzah!
    - Screen resolution is off, grid should be bigger

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,93 @@
 import { useCallback, useEffect, useMemo, useRef } from "react";
-import doomUrl from "./assets/doom.wasm?url";
 import "./App.css";
 import { DoomGridModel } from "./DoomGridModel";
 import { Grid, GridThemeType } from "@deephaven/grid";
 
 function App() {
-  const memory = useMemo(() => new WebAssembly.Memory({ initial: 108 }), []);
-  const canvas = useRef<HTMLCanvasElement>(null);
+  // const memory = useMemo(() => new WebAssembly.Memory({ initial: 108 }), []);
+  const gridRef = useRef<Grid>(null);
   const width = 640;
   const height = 400;
   const model = useMemo(() => new DoomGridModel({ width, height }), []);
 
-  const readWasmString = useCallback(
-    (offset: number, length: number) => {
-      const bytes = new Uint8Array(memory.buffer, offset, length);
-      return new TextDecoder("utf8").decode(bytes);
-    },
-    [memory]
-  );
+  // const readWasmString = useCallback(
+  //   (offset: number, length: number) => {
+  //     const bytes = new Uint8Array(memory.buffer, offset, length);
+  //     return new TextDecoder("utf8").decode(bytes);
+  //   },
+  //   [memory]
+  // );
 
-  const consoleLogString = useCallback(
-    (offset: number, length: number) => {
-      const string = readWasmString(offset, length);
-      console.log('"' + string + '"');
-    },
-    [readWasmString]
-  );
+  // const consoleLogString = useCallback(
+  //   (offset: number, length: number) => {
+  //     const string = readWasmString(offset, length);
+  //     console.log('"' + string + '"');
+  //   },
+  //   [readWasmString]
+  // );
 
-  const drawScreen = useCallback(
-    (ptr) => {
-      console.log("in draw screen");
-      const rawData = new Uint8ClampedArray(
-        memory.buffer,
-        ptr,
-        width * height * 4
-      );
-      const imageData = new ImageData(rawData, width, height);
+  // const drawScreen = useCallback(
+  //   (ptr) => {
+  //     console.log("in draw screen");
+  //     const rawData = new Uint8ClampedArray(
+  //       memory.buffer,
+  //       ptr,
+  //       width * height * 4
+  //     );
+  //     const imageData = new ImageData(rawData, width, height);
 
-      const canvasContext = canvas.current?.getContext("2d");
-      canvasContext?.putImageData(imageData, 0, 0);
+  //     const canvasContext = canvas.current?.getContext("2d");
+  //     canvasContext?.putImageData(imageData, 0, 0);
 
-      console.log("drawCanvas", ptr);
-    },
-    [memory.buffer]
-  );
+  //     console.log("drawCanvas", ptr);
+  //   },
+  //   [memory.buffer]
+  // );
 
-  const importObject = useMemo(
-    () => ({
-      env: { memory },
-      js: {
-        js_console_log: consoleLogString,
-        js_stdout: consoleLogString,
-        js_stderr: consoleLogString,
-        js_milliseconds_since_start: () => performance.now(),
-        js_draw_screen: drawScreen,
-      },
-    }),
-    [consoleLogString, drawScreen, memory]
-  );
+  // const importObject = useMemo(
+  //   () => ({
+  //     env: { memory },
+  //     js: {
+  //       js_console_log: consoleLogString,
+  //       js_stdout: consoleLogString,
+  //       js_stderr: consoleLogString,
+  //       js_milliseconds_since_start: () => performance.now(),
+  //       js_draw_screen: drawScreen,
+  //     },
+  //   }),
+  //   [consoleLogString, drawScreen, memory]
+  // );
 
+  // useEffect(() => {
+  //   (async () => {
+  //     const obj = await WebAssembly.instantiateStreaming(
+  //       fetch(doomUrl),
+  //       importObject
+  //     );
+  //     console.log("Obj is", obj);
+  //     console.log("instance is", obj.instance);
+  //     console.log("module is ", obj.module);
+  //     console.log("exports is", obj.instance.exports);
+  //     obj.instance.exports.main();
+
+  //     function step() {
+  //       obj.instance.exports.doom_loop_step();
+  //       window.requestAnimationFrame(step);
+  //     }
+  //     window.requestAnimationFrame(step);
+  //   })();
+  // }, [importObject]);
+
+  /** Start the draw loop */
   useEffect(() => {
     (async () => {
-      const obj = await WebAssembly.instantiateStreaming(
-        fetch(doomUrl),
-        importObject
-      );
-      console.log("Obj is", obj);
-      console.log("instance is", obj.instance);
-      console.log("module is ", obj.module);
-      console.log("exports is", obj.instance.exports);
-      obj.instance.exports.main();
-
       function step() {
-        obj.instance.exports.doom_loop_step();
+        gridRef.current?.forceUpdate();
         window.requestAnimationFrame(step);
       }
       window.requestAnimationFrame(step);
     })();
-  }, [importObject]);
+  }, []);
 
   const keyHandlers = useMemo(() => {
     return [model.keyHandler];
@@ -105,9 +115,9 @@ function App() {
 
   return (
     <>
-      <canvas width={width} height={height} ref={canvas} />
       <div style={{ width, height, position: "relative" }}>
         <Grid
+          ref={gridRef}
           model={model}
           keyHandlers={keyHandlers}
           mouseHandlers={mouseHandlers}
--- src/DoomGridModel.ts
+++ src/DoomGridModel.ts
@@ -4,10 +4,47 @@ import {
   KeyHandler,
   NullableGridColor,
 } from "@deephaven/grid";
+import doomUrl from "./assets/doom.wasm?url";
 import { DoomKeyHandler } from "./DoomKeyHandler";
 import { GridKeyboardEvent } from "@deephaven/grid/dist/KeyHandler";
 import { DoomMetricCalculator } from "./DoomMetricCalculator";
 import NullMouseHandler from "./NullMouseHandler";
+
+type DoomInstance = {
+  main: () => void;
+  add_browser_event: (event: number, keyCode: number) => void;
+  doom_loop_step: () => void;
+};
+
+function getDoomKeyCode(event: GridKeyboardEvent): number {
+  const { keyCode } = event;
+
+  // Doom seems to use mostly the same keycodes, except for the following (maybe I'm missing a few.)
+  switch (keyCode) {
+    case 8:
+      return 127; // KEY_BACKSPACE
+    case 17:
+      return 0x80 + 0x1d; // KEY_RCTRL
+    case 18:
+      return 0x80 + 0x38; // KEY_RALT
+    case 37:
+      return 0xac; // KEY_LEFTARROW
+    case 38:
+      return 0xad; // KEY_UPARROW
+    case 39:
+      return 0xae; // KEY_RIGHTARROW
+    case 40:
+      return 0xaf; // KEY_DOWNARROW
+    default:
+      if (keyCode >= 65 /*A*/ && keyCode <= 90 /*Z*/) {
+        return keyCode + 32; // ASCII to lower case
+      }
+      if (keyCode >= 112 /*F1*/ && keyCode <= 123 /*F12*/) {
+        return keyCode + 75; // KEY_F1
+      }
+      return keyCode;
+  }
+}
 
 /**
  * Play Doom in the Browser using @deephaven/grid with the DoomGridModel!
@@ -15,6 +52,10 @@ import NullMouseHandler from "./NullMouseHandler";
 export class DoomGridModel extends GridModel {
   private width: number;
   private height: number;
+  private memory: WebAssembly.Memory;
+  private imageData: ImageData | null = null;
+  private doomInstance: DoomInstance | null = null;
+
   readonly keyHandler: KeyHandler;
   readonly metricCalculator: DoomMetricCalculator;
   readonly mouseHandler: GridMouseHandler;
@@ -28,11 +69,75 @@ export class DoomGridModel extends GridModel {
     super();
     this.width = width;
     this.height = height;
+    this.consoleErrorString = this.consoleErrorString.bind(this);
+    this.consoleLogString = this.consoleLogString.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.handleDrawScreen = this.handleDrawScreen.bind(this);
     this.keyHandler = new DoomKeyHandler(this.handleKeyDown, this.handleKeyUp);
     this.metricCalculator = new DoomMetricCalculator();
     this.mouseHandler = new NullMouseHandler();
+    this.memory = new WebAssembly.Memory({ initial: 108 });
+
+    this.initDoom();
+  }
+
+  private async initDoom() {
+    const response = await fetch(doomUrl);
+    const wasm = await WebAssembly.instantiateStreaming(response, {
+      env: { memory: this.memory },
+      js: {
+        js_console_log: this.consoleLogString,
+        js_stdout: this.consoleLogString,
+        js_stderr: this.consoleLogString,
+        js_milliseconds_since_start: () => performance.now(),
+        js_draw_screen: this.handleDrawScreen,
+      },
+    });
+    console.log("Obj is", wasm);
+    console.log("instance is", wasm.instance);
+    console.log("module is ", wasm.module);
+    console.log("exports is", wasm.instance.exports);
+
+    const doomInstance = wasm.instance.exports as DoomInstance;
+    doomInstance.main();
+
+    this.doomInstance = doomInstance;
+
+    function step() {
+      // console.log("step");
+      doomInstance.doom_loop_step();
+      window.requestAnimationFrame(step);
+    }
+    window.requestAnimationFrame(step);
+  }
+
+  private readWasmString(offset: number, length: number): string {
+    const bytes = new Uint8Array(this.memory.buffer, offset, length);
+    return new TextDecoder("utf8").decode(bytes);
+  }
+
+  private consoleLogString(offset: number, length: number): void {
+    const string = this.readWasmString(offset, length);
+    console.log('"' + string + '"');
+  }
+
+  private consoleErrorString(offset: number, length: number): void {
+    const string = this.readWasmString(offset, length);
+    console.error('"' + string + '"');
+  }
+
+  private handleDrawScreen(ptr: number): void {
+    // console.log("in draw screen");
+    const rawData = new Uint8ClampedArray(
+      this.memory.buffer,
+      ptr,
+      this.width * this.height * 4
+    );
+
+    this.imageData = new ImageData(rawData, this.width, this.height);
+
+    // console.log("this.imageData is", this.imageData.data);
   }
 
   get rowCount(): number {
@@ -52,14 +157,42 @@ export class DoomGridModel extends GridModel {
   }
 
   backgroundColorForCell(column: number, row: number): NullableGridColor {
-    return "red";
+    // const frame = context.getImageData(0, 0, width, height);
+    // const l = frame.data.length / 4;
+
+    // for (let i = 0; i < l; i++) {
+    //   const grey =
+    //     (frame.data[i * 4 + 0] +
+    //       frame.data[i * 4 + 1] +
+    //       frame.data[i * 4 + 2]) /
+    //     3;
+
+    //   frame.data[i * 4 + 0] = grey;
+    //   frame.data[i * 4 + 1] = grey;
+    //   frame.data[i * 4 + 2] = grey;
+    // }
+
+    // Need to get the color from the imageData. It's a one dimensional array of RGBA values, from top-left to bottom-right.
+    // The color for the cell at column, row is at index (row * width + column) * 4.
+    // The color for the red channel is at index (row * width + column) * 4.
+    // The color for the green channel is at index (row * width + column) * 4 + 1.
+    // The color for the blue channel is at index (row * width + column) * 4 + 2.
+    // The color for the alpha channel is at index (row * width + column) * 4 + 3.
+    // return "red";
+    // console.log("Image data blah", this.imageData);
+    const r = this.imageData?.data[(row * this.width + column) * 4];
+    const g = this.imageData?.data[(row * this.width + column) * 4 + 1];
+    const b = this.imageData?.data[(row * this.width + column) * 4 + 2];
+    return `rgba(${r}, ${g}, ${b}, 255)`;
   }
 
   private handleKeyDown(event: GridKeyboardEvent) {
     console.log("Key down", event.key);
+    this.doomInstance?.add_browser_event(0, getDoomKeyCode(event));
   }
 
   private handleKeyUp(event: GridKeyboardEvent) {
     console.log("Key up", event.key);
+    this.doomInstance?.add_browser_event(1, getDoomKeyCode(event));
   }
 }
