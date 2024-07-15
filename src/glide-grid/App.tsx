2022c44ba96f3e0c0c886f32a7d1142aafc72aae
Mike Bender <mikebender@deephaven.io>
2024-07-15 15:43:28 -0400

    Use custom renderer for drawing the cell background for Glide
    - Much better performance, but still not as good as their site
    - Looks like their site limits fps - we want maximum FPS for our benchmarks though

--- src/glide-grid/App.tsx
+++ src/glide-grid/App.tsx
@@ -3,12 +3,10 @@ import {
   CustomRenderer,
   DataEditor,
   DataEditorRef,
-  DrawCellCallback,
   GridCell,
   GridCellKind,
   GridColumn,
   Item,
-  TextCell,
 } from "@glideapps/glide-data-grid";
 import "@glideapps/glide-data-grid/dist/index.css";
 import { useCallback, useEffect, useMemo, useRef } from "react";
@@ -31,9 +29,6 @@ function App() {
         row.push({
           kind: GridCellKind.Custom,
           copyData: "",
-          // themeOverride: {
-          //   bgCell: "transparent",
-          // },
           data: "", // Will fill in with the color of the cell when it's available
           allowOverlay: false,
         });
@@ -41,17 +36,6 @@ function App() {
       newData.push(row);
     }
     return newData;
-  }, []);
-
-  const cellUpdates = useMemo(() => {
-    const updates: { cell: Item }[] = [];
-
-    for (let r = 0; r < height; r++) {
-      for (let c = 0; c < width; c++) {
-        updates.push({ cell: [c, r] });
-      }
-    }
-    return updates;
   }, []);
 
   const columns: GridColumn[] = Array.from({ length: width }, (_, i) => ({
@@ -73,6 +57,7 @@ function App() {
         // Update the rowData with the new image data
         const { data } = imageData;
         const scale = imageData.width / width;
+        const newUpdates: { cell: Item }[] = [];
         for (let r = 0; r < height; r++) {
           const row = gridData[r];
           for (let c = 0; c < width; c++) {
@@ -80,12 +65,17 @@ function App() {
             const color = `rgba(${data[colorIndex]}, ${data[colorIndex + 1]}, ${
               data[colorIndex + 2]
             })`;
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            (row[c] as any).data = color;
-            // row[c].themeOverride!.bgCell = color;
+            const cell = row[c] as RenderCell;
+            if (cell.data !== color) {
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              (cell as any).data = color;
+              newUpdates.push({ cell: [c, r] });
+            }
           }
         }
-        gridRef.current?.updateCells(cellUpdates);
+        if (newUpdates.length > 0) {
+          gridRef.current?.updateCells(newUpdates);
+        }
       });
       doom.start();
       window.addEventListener("keydown", doom.keyDown);
@@ -96,17 +86,11 @@ function App() {
         window.removeEventListener("keyup", doom.keyUp);
       };
     },
-    [cellUpdates, gridData]
+    [gridData]
   );
 
-  const drawCell: DrawCellCallback = useCallback(({ ctx, cell, rect }) => {
-    ctx.fillStyle = (cell as RenderCell).data as string;
-    ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
-    // console.log("xxx drawCell", cell, rect);
-  }, []);
-
   const experimentalOptions = useMemo(() => {
-    return { disableMinimumCellWidth: true };
+    return { disableAccessibilityTree: true, disableMinimumCellWidth: true };
   }, []);
 
   const cellRenderer: CustomRenderer<RenderCell> = useMemo(() => {
@@ -117,7 +101,6 @@ function App() {
         const { ctx, rect } = args;
         ctx.fillStyle = (cell as RenderCell).data as string;
         ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
-        // console.log("xxx cellRenderer draw", cell, rect);
       },
     };
   }, []);
@@ -135,7 +118,6 @@ function App() {
       }}
     >
       <DataEditor
-        drawCell={drawCell}
         getCellContent={getCell}
         customRenderers={customRenderers}
         experimental={experimentalOptions}
